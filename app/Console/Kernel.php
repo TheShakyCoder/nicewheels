1cb4512a9694bcf17ceb649f903ab9f57a65134d
Sharif Khan <support@fig.limited>
2021-08-17 19:27:32 +0100

    ebay:poll 30 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('ebay:poll')->hourly();
+        $schedule->command('ebay:poll')->everyThirtyMinutes();
         $schedule->command('ebay:get')->everyThirtyMinutes();
         $schedule->command('ebay:finish')->everyFifteenMinutes();
         $schedule->command('static:filter')->daily();
