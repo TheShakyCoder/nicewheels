a5150a2efc9c7c2c3a0cfc0d103cd9f49a339a6c
Sharif Khan <support@fig.limited>
2021-08-24 07:58:42 +0100

    cache iamges for 24 hours

--- app/Http/Controllers/Api/CarImagesController.php
+++ app/Http/Controllers/Api/CarImagesController.php
@@ -3,16 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\EbayItemImage;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
+use App\Models\EbayItemImage;
 
 class CarImagesController extends Controller
 {
     public function show(EbayItemImage $ebayItemImage)
     {
-        return \Storage::disk('spaces')->temporaryUrl(
-            config('filesystems.disks.spaces.folder').'/ebay-items/'.$ebayItemImage->ebay_item_id.'/'.$ebayItemImage->file,
-            now()->addMinutes(60)
-        );
+        // $image = \Storage::disk('spaces')->temporaryUrl(
+        //     config('filesystems.disks.spaces.folder').'/ebay-items/'.$ebayItemImage->ebay_item_id.'/'.$ebayItemImage->file,
+        //     now()->addMinutes(60)
+        // );
+                
+        $hours = 24;
+
+        $value = Cache::remember('image-'.$ebayItemImage->id, 3600 * $hours, function () use ($hours, $ebayItemImage) {
+            return \Storage::disk('spaces')->temporaryUrl(
+                config('filesystems.disks.spaces.folder').'/ebay-items/'.$ebayItemImage->ebay_item_id.'/'.$ebayItemImage->file,
+                now()->addMinutes(60)
+            ); 
+        });
+
+        return $value;
+
+        // return \Storage::disk('spaces')->temporaryUrl(
+        //     config('filesystems.disks.spaces.folder').'/ebay-items/'.$ebayItemImage->ebay_item_id.'/'.$ebayItemImage->file,
+        //     now()->addMinutes(60)
+        // );
     }
 }
